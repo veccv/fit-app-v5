ec40007716f865e920bb2aa81b8ecefe8424b899
Jacek Dalmer <dalmerjack@wp.pl>
2023-12-25 10:58:00 +0100

    init commit

---
+++ src/main/java/com/github/veccvs/fitappv5/FitAppV5Application.java
@@ -0,0 +1,12 @@
+package com.github.veccvs.fitappv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitAppV5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FitAppV5Application.class, args);
+  }
+}
